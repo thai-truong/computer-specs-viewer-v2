f924b080c3a015155859a86b815617869da72978
thai-truong <ttgilles@ucsd.edu>
2022-02-09 22:15:08 -0800

    Project init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.computerspecsviewer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.computerspecsviewer to javafx.fxml;
+    exports com.example.computerspecsviewer;
+}
