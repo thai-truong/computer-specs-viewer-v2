7fef5c7202dec5f52789fccd939b595fa9a3d41d
thai-truong <ttgilles@ucsd.edu>
2022-02-19 16:59:51 -0800

    Add display info query, remove redundant whitespace from CustomSingleton's start string

---
+++ src/main/java/org/computerspecsviewer/infoquery/display/DisplayInfo.java
@@ -0,0 +1,33 @@
+package org.computerspecsviewer.infoquery.display;
+
+import org.computerspecsviewer.displaytypes.customboolean.CustomBoolean;
+import org.computerspecsviewer.displaytypes.customfield.CustomField;
+import org.computerspecsviewer.displaytypes.customsingleton.CustomSingleton;
+import org.computerspecsviewer.infoquery.base.BaseInfoQuery;
+import oshi.hardware.Display;
+import oshi.util.EdidUtil;
+
+public class DisplayInfo extends BaseInfoQuery {
+    public String productId;
+    public String serialNumber;
+    public String manufacturerId;
+    public int yearManufactured;
+    public CustomField<Integer> monitorHeight;
+    public CustomField<Integer> monitorWidth;
+    public CustomBoolean isDigital;
+    public CustomSingleton<DisplayDescriptors> displayDescriptors;
+
+    public DisplayInfo(Display displayDevice) {
+        final String CM = "cm";
+        byte[] edidInfo = displayDevice.getEdid();
+
+        productId = EdidUtil.getProductID(edidInfo);
+        serialNumber = EdidUtil.getSerialNo(edidInfo);
+        manufacturerId = EdidUtil.getManufacturerID(edidInfo);
+        yearManufactured = EdidUtil.getYear(edidInfo);
+        monitorHeight = new CustomField<>(EdidUtil.getVcm(edidInfo), CM);
+        monitorWidth = new CustomField<>(EdidUtil.getHcm(edidInfo), CM);
+        isDigital = new CustomBoolean(EdidUtil.isDigital(edidInfo), "Yes", "No (Analog)");
+        displayDescriptors = new CustomSingleton<>(new DisplayDescriptors(edidInfo));
+    }
+}
---
+++ src/main/java/org/computerspecsviewer/infoquery/display/DisplayInfoQuery.java
@@ -0,0 +1,39 @@
+package org.computerspecsviewer.infoquery.display;
+
+import org.computerspecsviewer.displaytypes.customlist.CustomList;
+import org.computerspecsviewer.infoquery.base.BaseInfoQuery;
+import org.computerspecsviewer.infoquery.singletons.SystemInfoSingleton;
+import oshi.SystemInfo;
+import oshi.hardware.Display;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.EdidUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayInfoQuery extends BaseInfoQuery {
+    public CustomList<DisplayInfo> displayDevices;
+
+    public DisplayInfoQuery() {
+        SystemInfo sysInfo = SystemInfoSingleton.getInstance();
+        HardwareAbstractionLayer hal = sysInfo.getHardware();
+        List<Display> displayList = hal.getDisplays();
+        List<DisplayInfo> displayInfoList = new ArrayList<>();
+
+        for(Display display: displayList) {
+            displayInfoList.add(new DisplayInfo(display));
+        }
+
+        displayDevices = new CustomList<>(displayInfoList, "Display Device", false);
+    }
+
+    @Override
+    public String toString() {
+        return displayDevices.toString();
+    }
+
+    public static void main(String[] args) {
+        DisplayInfoQuery test = new DisplayInfoQuery();
+        System.out.println(test);
+    }
+}
